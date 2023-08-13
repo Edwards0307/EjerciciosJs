59e233d879cbf26d7e3a5e9d3f8b014cd31c90a5
Edwards0307 <edwards.ardila03@gmail.com>
2023-08-13 17:31:29 -0500

    First commit

---
+++ js/repaso1.js
@@ -0,0 +1,10 @@
+/**
+ * 1 - 
+ * Ingresar un departamento (Cundinamarca, quindio, antioquia, valle)
+ * alert("opcion no valida")
+ * Dependiendo de que departamento es debe mostrar cual es la capital
+ */
+
+/**
+ * 2 - 
+ */
